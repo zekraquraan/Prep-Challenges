ecc5adfc0c287a38b9a8d289cb01fad3df2c5abc
zekraquraan <zekraquraan7@gmail.com>
2023-04-04 11:32:29 +0000

    updated

--- code-challenges/challenges-16.js
+++ code-challenges/challenges-16.js
@@ -47,6 +47,12 @@ const detectFace = (arr) => {
 
 const eveCharacter = (str) => {
     //write your code here ...
+    let result = "";
+    for (let i = 0; i < str.length; i += 2) {
+      result += str[i];
+    }
+    return result;
+
 }
 // -------------------------------------------------------------------------------------------------------
 
